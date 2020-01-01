2239a9954a6d8c9778f8da59357a12faa8b3ce3b
Sajal <sirohisajal@gmail.com>
2020-01-01 22:33:23 +0530

    Add files via upload

---
+++ price-component-layout-toggle/js/my.js
@@ -0,0 +1,8 @@
+// $('input[type=checkbox]').click(function () {
+//     if ($('input[type=checkbox]').is(':checked')) {
+//         $('#prof-amount').val('564.332');
+//     }
+//     else{
+//         $('#txt').val('unchecked');
+//     }
+// });
